f9c76566963d68cbff18f307c1f29c5f822c5e61
Orpaz Goldstein <orpgol@gmail.com>
2017-02-19 18:59:42 -0800

    Predictor ready

---
+++ lasso_predictor.py
@@ -0,0 +1,46 @@
+
+# coding: utf-8
+
+# In[7]:
+
+import numpy as np
+import pandas as pd
+import pickle
+import sys
+
+import sklearn as sl
+from sklearn.externals import joblib
+
+
+# In[8]:
+
+def main():
+    lasso = joblib.load('model.pkl') 
+    test = pd.read_csv(sys.argv[1],index_col=False)
+
+    test["jump"] = np.log(test["jump"])
+    numerical_data = ['age', 'height', 'weight', 'sleep']
+    test[numerical_data] = np.log1p(test[numerical_data])
+    test = pd.get_dummies(test, columns=['exercise', 'competitive', 'gender', 'injured', 'color', 'race'])
+
+    X_test = test.drop('jump', axis=1)
+    y = test.jump
+
+    lasso.predict(X_test)
+    print('Prediction Accuracy:')
+    print(lasso.score(X_test,y))
+
+    res = lasso.predict(X_test)
+    print('Predicted Resutls:')
+    print(np.exp(res))
+
+
+# In[ ]:
+
+if __name__ == '__main__':
+    if len(sys.argv) <= 1:
+        print('Please enter a file name for the test case')
+        print('Usage: python lasso_predictor test.csv')
+    else:
+        main()
+
